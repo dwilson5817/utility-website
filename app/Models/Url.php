b172886013f6f9d50361c5569b8b3ef52842ee7c
Dylan Wilson <dwilson5817@gmail.com>
2021-08-15 10:38:08 -0700

    [#2] Create URL model
    Create the URL model to represent a shortened URL in the database.  The
    URL model will store the full link as well as the slug of the shortened
    URL.  It also includes the user who shortened the URL (where NULL
    represents an unauthenticated user) and the total amount of visits the
    shortened link has received.
    
    Closes #5.

---
+++ app/Models/Url.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Url extends Model
+{
+    use HasFactory;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     * Generate a unique, 6 character slug for the short URL.
+     *
+     * @return string
+     */
+    public static function generateSlug()
+    {
+        // Create the slug variable to store the slug to
+        $slug = null;
+
+        // While the slug is null we loop through this code block
+        while ($slug == null) {
+
+            // Get a random 6 digit alphanumeric string
+            $slug = Str::random(6);
+
+            // Check if the randomly selected slug is already in use
+            if (Url::where('slug', $slug)->get()->isNotEmpty()) {
+
+                // If the slug is in use, we can't use it again so we assign it to null to cause another loop
+                $slug = null;
+            }
+        }
+
+        // We've generated the new slug, so return it
+        return $slug;
+    }
+}
